d64b279291d5073b3712b53b7d91ba8a673867e2
lucaslima44 <lucaslima2011h@gmail.com>
2024-10-12 23:03:06 -0300

    add: challenge finished

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "11999999999", modelo: "Modelo 1", imei: "IMEI 1", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snapchat");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "11999999999", modelo: "Modelo 1", imei: "IMEI 1", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
